c65a114f9dec84ca10fd267763c6a3546b442018
Maria Andreyeva <maria_sheleg@mail.ru>
2017-09-26 22:37:59 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    var a = first.split('').reverse();
+    var b = second.split('').reverse();
+    var sizeA = a.length;
+    var sizeB = b.length;
+    var result = new Array(sizeA + sizeB).fill(0);
+
+    for (var i = 0; i < sizeA; i++) {
+        for (var j = 0; j < sizeB; j++) {
+            result[i + j] += a[i] * b[j];
+        }
+
+        for (k = 0; k < result.length - 1; k++) {
+            result[k + 1] += Math.trunc(result[k] / 10);
+            result[k] = result[k] % 10;
+        }
+    }
+
+    if (result[result.length - 1] === 0) {
+        result.pop();
+    }
+
+    return result.reverse().join('');
 }
